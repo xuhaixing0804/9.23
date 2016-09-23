9c6bfb04cacb3f9ed436c6599669a78a0926dd21
xhx <xhx@example.com>
2016-09-23 14:18:57 +0800

    branch dev1

---
+++ 9.23.js
@@ -0,0 +1,24 @@
+(function(){
+  var Button1 = document.getElementsByTagName("button")[0];
+  console.log(Button1);
+  var Buttons = document.getElementsByTagName("button");
+  console.log(Buttons);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}(window))
